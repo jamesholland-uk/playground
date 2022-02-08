9f61c310f7892f592e2923386cd089acf2ec9f57
James Holland <jamoiholland@gmail.com>
2022-02-08 12:31:58 +0000

    Add DP NICs and bootstrap

--- azure-single-ngfw/main.tf
+++ azure-single-ngfw/main.tf
@@ -23,29 +23,33 @@ module "vnet" {
   location             = var.location
   resource_group_name  = azurerm_resource_group.this.name
   address_space        = ["10.110.0.0/16"]
-  network_security_groups = {
-    "management-security-group" = {
-      rules = {
-        "vm-management-rules" = {
-          access                     = "Allow"
-          direction                  = "Inbound"
-          priority                   = 100
-          protocol                   = "TCP"
-          source_port_range          = "*"
-          source_address_prefixes    = var.allow_inbound_mgmt_ips
-          destination_address_prefix = "*"
-          destination_port_range     = "*"
-        }
-      }
-    }
-  }
+  network_security_groups = var.network_security_groups
   route_tables = {}
   subnets = {
     "subnet-mgmt" = {
       address_prefixes       = ["10.110.255.0/24"]
-      network_security_group = "management-security-group"
+      network_security_group = "sg-mgmt"
+    }
+    "subnet-private" = {
+      address_prefixes       = ["10.110.0.0/24"]
+      network_security_group = "sg-private"
+    }
+    "subnet-public" = {
+      address_prefixes       = ["10.110.129.0/24"]
+      network_security_group = "sg-public"
     }
   }
+}
+
+# The VM-Series bootstrap
+module "bootstrap" {
+  source  = "PaloAltoNetworks/vmseries-modules/azurerm//modules/bootstrap"
+  version = "0.2.0"
+
+  resource_group_name  = azurerm_resource_group.this.name
+  location             = azurerm_resource_group.this.location
+  storage_account_name = var.storage_account_name
+  files                = var.files
 }
 
 # The VM-Series virtual machine
@@ -66,5 +70,18 @@ module "vmseries" {
       subnet_id        = lookup(module.vnet.subnet_ids, "subnet-mgmt", null)
       create_public_ip = true
     },
+    {
+      name                = "public-interface"
+      subnet_id           = lookup(module.vnet.subnet_ids, "subnet-public", null)
+      create_public_ip    = true
+      enable_backend_pool = false
+    },
+    {
+      name                = "inside-interface"
+      subnet_id           = lookup(module.vnet.subnet_ids, "subnet-private", null)
+      enable_backend_pool = false
+    },
   ]
+  bootstrap_storage_account     = module.bootstrap.storage_account
+  bootstrap_share_name          = module.bootstrap.storage_share.name
 }
