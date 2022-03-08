d0eebe745fdcaaa15057bf99bb2ab2395a9f713b
James Holland <jamoiholland@gmail.com>
2022-03-08 19:52:30 +0000

    Checkov tests

---
+++ panos-terraform-stuff/homelabrama/tunnel_int.tf
@@ -0,0 +1,15 @@
+# resource "panos_panorama_tunnel_interface" "gcp_tunnel" {
+#   template = "lab-vm-series"
+#   name = "tunnel.8"
+#   comment = "Tunnel for GCP demo"
+
+#   lifecycle {
+#       create_before_destroy = true
+#   }
+# }
+
+# data "panos_system_info" "device_info" { }
+
+# output "model" {
+#     value = data.panos_system_info.device_info.info.model
+# }
