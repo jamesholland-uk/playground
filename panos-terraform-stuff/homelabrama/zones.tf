793d38fc348703c1878a3457dc89c67ed9b3dd91
James Holland <jamoiholland@gmail.com>
2022-03-14 14:31:14 +0000

    More Checkov testing

--- panos-terraform-stuff/homelabrama/zones.tf
+++ panos-terraform-stuff/homelabrama/zones.tf
@@ -1,59 +1,129 @@
-# The "zone_profile" attributes can be defined in either the "panos_zone", "panos_zone_entry" or "panos_panorama_zone" resources.
-# All three resource types are covered by this check.
+# # The "zone_profile" attributes can be defined in either the "panos_zone" or "panos_panorama_zone" resources.
+# # All these resource types are covered by this check.
 
-# Passes
+# # Passes
 
-# Zones should should have a "zone_profile" populated to protect against provide extended protection against IP floods, reconnaissance, packet based attacks, etc
-resource "panos_zone" "pass1" {
-    name = "new_zone"
-    zone_profile = "zone_protect_profile"
-}
-resource "panos_zone_entry" "pass2" {
-    name = "new_zone_entry"
-    zone_profile = "zone_protect_profile"
-}
-resource "panos_panorama_zone" "pass3" {
-    name = "new_zone_panorama"
-    zone_profile = "zone_protect_profile"
-}
+# # Zones should should have a "zone_profile" populated to protect against provide extended protection against IP floods, reconnaissance, packet based attacks, etc
+# resource "panos_zone" "pass1" {
+#     name = "new_zone"
+#     zone_profile = "zone_protect_profile"
+# }
+# resource "panos_panorama_zone" "pass2" {
+#     name = "new_zone_panorama"
+#     zone_profile = "zone_protect_profile"
+# }
 
-# Fails
+# # Fails
 
-# Zones should should have a "zone_profile" populated to protect against provide extended protection against IP floods, reconnaissance, packet based attacks, etc - lack of "zone_profile" attribute is a fail
-resource "panos_zone" "fail1" {
-    name = "new_zone"
-}
-resource "panos_zone_entry" "fail2" {
-    name = "new_zone_entry"
-}
-resource "panos_panorama_zone" "fail3" {
-    name = "new_zone_panorama"
-}
+# # Zones should should have a "zone_profile" populated to protect against provide extended protection against IP floods, reconnaissance, packet based attacks, etc - lack of "zone_profile" attribute is a fail
+# resource "panos_zone" "fail1" {
+#     name = "new_zone"
+# }
+# resource "panos_panorama_zone" "fail2" {
+#     name = "new_zone_panorama"
+# }
 
-# Zones should should have a "zone_profile" populated to protect against provide extended protection against IP floods, reconnaissance, packet based attacks, etc - empty string "zone_profile" attributes are a fail
-resource "panos_zone" "fail4" {
-    name = "new_zone"
-    zone_profile = ""
-}
-resource "panos_zone_entry" "fail5" {
-    name = "new_zone_entry"
-    zone_profile = ""
-}
-resource "panos_panorama_zone" "fail6" {
-    name = "new_zone_panorama"
-    zone_profile = ""
-}
+# # Zones should should have a "zone_profile" populated to protect against provide extended protection against IP floods, reconnaissance, packet based attacks, etc - empty string "zone_profile" attributes are a fail
+# resource "panos_zone" "fail3" {
+#     name = "new_zone"
+#     zone_profile = ""
+# }
+# resource "panos_panorama_zone" "fail4" {
+#     name = "new_zone_panorama"
+#     zone_profile = ""
+# }
 
-# Zones should should have a "zone_profile" populated to protect against provide extended protection against IP floods, reconnaissance, packet based attacks, etc - strings of space characters for "zone_profile" attributes are a fail
-resource "panos_zone" "fail7" {
-    name = "new_zone"
-    zone_profile = "   "
-}
-resource "panos_zone_entry" "fail8" {
-    name = "new_zone_entry"
-    zone_profile = "   "
-}
-resource "panos_panorama_zone" "fail9" {
-    name = "new_zone_panorama"
-    zone_profile = "   "
-}
+# # Zones should should have a "zone_profile" populated to protect against provide extended protection against IP floods, reconnaissance, packet based attacks, etc - strings of space characters for "zone_profile" attributes are a fail
+# resource "panos_zone" "fail5" {
+#     name = "new_zone"
+#     zone_profile = "   "
+# }
+# resource "panos_panorama_zone" "fail6" {
+#     name = "new_zone_panorama"
+#     zone_profile = "   "
+# }
+
+
+# # If User-ID is enabled for a zone (optional), an "include ACL" should be defined to provide scope for User-ID. This can be configured in "panos_zone" or "panos_panorama_zone" resources. Both resource types are covered by this check.
+
+# # Passes
+
+# # User-ID enabled, Include ACL defined, single entry in list
+# resource "panos_zone" "pass1" {
+#     name = "new_zone"
+#     zone_profile = "zone_protect_profile"
+#     enable_user_id = true
+#     include_acls = ["10.0.0.0./8"]
+# }
+# resource "panos_panorama_zone" "pass2" {
+#     name = "new_zone"
+#     zone_profile = "zone_protect_profile"
+#     enable_user_id = true
+#     include_acls = ["10.0.0.0./8"]
+# }
+
+# # User-ID enabled, Include ACL defined, double entry in list
+# resource "panos_zone" "pass3" {
+#     name = "new_zone"
+#     zone_profile = "zone_protect_profile"
+#     enable_user_id = true
+#     include_acls = ["10.0.0.0./8", "192.168.0.0/16"]
+# }
+# resource "panos_panorama_zone" "pass4" {
+#     name = "new_zone"
+#     zone_profile = "zone_protect_profile"
+#     enable_user_id = true
+#     include_acls = ["10.0.0.0./8", "192.168.0.0/16"]
+# }
+
+# # User-ID not enabled, Include ACL not required
+# resource "panos_zone" "pass5" {
+#     name = "new_zone"
+#     zone_profile = "zone_protect_profile"
+# }
+# resource "panos_panorama_zone" "pass6" {
+#     name = "new_zone"
+#     zone_profile = "zone_protect_profile"
+# }
+
+# # Fails
+
+# # User-ID enabled, Include ACL undefined
+# resource "panos_zone" "fail1" {
+#     name = "new_zone"
+#     zone_profile = "zone_protect_profile"
+#     enable_user_id = true
+# }
+# resource "panos_panorama_zone" "fail2" {
+#     name = "new_zone"
+#     zone_profile = "zone_protect_profile"
+#     enable_user_id = true
+# }
+
+# # User-ID enabled, Include ACL defined, empty string in list
+# resource "panos_zone" "fail3" {
+#     name = "new_zone"
+#     zone_profile = "zone_protect_profile"
+#     enable_user_id = true
+#     include_acls = [""]
+# }
+# resource "panos_panorama_zone" "fail4" {
+#     name = "new_zone"
+#     zone_profile = "zone_protect_profile"
+#     enable_user_id = true
+#     include_acls = [""]
+# }
+
+# # User-ID enabled, Include ACL defined, string of spaces in list
+# resource "panos_zone" "fail5" {
+#     name = "new_zone"
+#     zone_profile = "zone_protect_profile"
+#     enable_user_id = true
+#     include_acls = ["    "]
+# }
+# resource "panos_panorama_zone" "fail6" {
+#     name = "new_zone"
+#     zone_profile = "zone_protect_profile"
+#     enable_user_id = true
+#     include_acls = ["    "]
+# }
