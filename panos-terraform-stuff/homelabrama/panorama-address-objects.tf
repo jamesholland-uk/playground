5c67497fc9a12963f66f58ec5d09a8382a36f21e
James Holland <jamoiholland@gmail.com>
2022-04-01 15:54:53 +0200

    Moar testing

--- panos-terraform-stuff/homelabrama/panorama-address-objects.tf
+++ panos-terraform-stuff/homelabrama/panorama-address-objects.tf
@@ -1,36 +1,28 @@
 # resource "panos_panorama_address_object" "terraform-address-object-1" {
-#     name = "terraform-address-object-1"
+#     name = "terraform-address-object-pt1"
 #     value = "192.168.80.1/32"
-#     description = "Addres object from Terraform"
+#     description = "Address object from Terraform"
+#     device_group = "lab-vm-series"
+#     lifecycle {
+#         create_before_destroy = true
+#         }
 # }
 
 # resource "panos_panorama_address_object" "terraform-address-object-2" {
 #     name = "terraform-address-object-pt2"
 #     value = "192.168.70.2/32"
-#     description = "Addres object from Terraform"
-# }
-
-# resource "panos_panorama_address_group" "terraform-address-group" {
-#     name = "terraform-address-group"
-#     static_addresses = [
-#         panos_panorama_address_object.terraform-address-object-1.name,
-#         panos_panorama_address_object.terraform-address-object-2.name
-#     ]
-# }
-
-# resource "panos_panorama_address_object" "terraform-address-object-1" {
-#     name = "terraform-address-object-1"
-#     value = "192.168.80.1/32"
-#     description = "Addres object from Terraform"
+#     description = "Address object from Terraform"
+#     device_group = "lab-vm-series"
 #     lifecycle {
 #         create_before_destroy = true
 #         }
 # }
 
-# resource "panos_panorama_address_object" "terraform-address-object-2" {
-#     name = "terraform-address-object-pt2"
-#     value = "192.168.80.2/32"
-#     description = "Addres object from Terraform"
+# resource "panos_panorama_address_object" "terraform-address-object-3" {
+#     name = "terraform-address-object-pt3"
+#     value = "192.168.70.3/32"
+#     description = "Address object from Terraform"
+#     device_group = "lab-vm-series"
 #     lifecycle {
 #         create_before_destroy = true
 #         }
@@ -42,6 +34,7 @@
 #         panos_panorama_address_object.terraform-address-object-1.name,
 #         panos_panorama_address_object.terraform-address-object-2.name
 #     ]
+#     device_group = "lab-vm-series"
 #     lifecycle {
 #         create_before_destroy = true
 #         }
